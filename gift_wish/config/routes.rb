385b7267b2219bdf44424aa86b5153e2057c4077
hsiaomb <hsiaomb@usc.edu>
2015-11-02 09:51:17 +0000

    Made model associations

--- gift_wish/config/routes.rb
+++ gift_wish/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :products
 
-  root "users#new"
+  root "pages#homepage"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
