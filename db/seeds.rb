3a3ae1c382787b8343244020ef0abae8f4715684
hsiaomb <hsiaomb@usc.edu>
2015-11-05 17:17:00 +0000

    added products to fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ Product.delete_all("title != 'Jordan Eclipse'")
 
 password = "password"
 
+users = []
+
 10.times do
 	user = User.create ({ 
 		first_name: Name.first_name,
@@ -20,53 +22,60 @@ password = "password"
 		dress_size: "",
 		password: password,
 		password_confirmation: password})
+	users << user
+	user = User.create ({ 
+		first_name: Name.first_name,
+		last_name: Name.last_name,
+		photo_url: "https://www.placecage.com/200/300",
+		username: Internet.user_name,
+		email: Internet.email,
+		gender: "Female",
+		shirt_size: "F",
+		birth_date: "1992/07/06",
+		shoe_size: "6.5",
+		waist_size: "8",
+		dress_size: "6",
+		password: password,
+		password_confirmation: password})
+	users << user
 
-		user = User.create ({ 
-			first_name: Name.first_name,
-			last_name: Name.last_name,
-			photo_url: "https://www.placecage.com/200/300",
-			username: Internet.user_name,
-			email: Internet.email,
-			gender: "Female",
-			shirt_size: "F",
-			birth_date: "1992/07/06",
-			shoe_size: "6.5",
-			waist_size: "8",
-			dress_size: "6",
-			password: password,
-			password_confirmation: password})
+	product = Product.create ({ 
+		title: Commerce.product_name,
+		category_id: Number.between(1, 9),
+		price: Number.decimal(3, 2),
+		product_url: "http://www.amazon.co.uk/",
+		photo: "http://ecx.images-amazon.com/images/I/61Su9Gvq-fL._SX522_.jpg"}),
+	user_id: users.sample.id
 
-		product = Product.create ({ 
-					title: Commerce.product_name,
-					category_id: Number.between(1, 9),
-					price: Number.decimal(3, 2),
-					product_url: "http://www.amazon.co.uk/",
-					photo: "http://ecx.images-amazon.com/images/I/61Su9Gvq-fL._SX522_.jpg"})
 
-		product = Product.create ({ 
-					title: Commerce.product_name,
-					category_id: Number.between(1, 9),
-					price: Number.decimal(3, 2),
-					product_url: "http://www.amazon.co.uk/",
-					photo: "http://ecx.images-amazon.com/images/I/819R4oLggwL._UY879_.jpg"})
+	product = Product.create ({ 
+		title: Commerce.product_name,
+		category_id: Number.between(1, 9),
+		price: Number.decimal(3, 2),
+		product_url: "http://www.amazon.co.uk/",
+		photo: "http://ecx.images-amazon.com/images/I/819R4oLggwL._UY879_.jpg"}),
+	user_id: users.sample.id
 
-		product = Product.create ({ 
-					title: Commerce.product_name,
-								category_id: Number.between(1, 9),
-								price: Number.decimal(3, 2),
-								product_url: "http://www.amazon.co.uk/",
-								photo: "http://ecx.images-amazon.com/images/I/41rMfUQjRcL._AC_SY220_.jpg"})
+	product = Product.create ({ 
+		title: Commerce.product_name,
+		category_id: Number.between(1, 9),
+		price: Number.decimal(3, 2),
+		product_url: "http://www.amazon.co.uk/",
+		photo: "http://ecx.images-amazon.com/images/I/41rMfUQjRcL._AC_SY220_.jpg"}),
+	user_id: users.sample.id
 
-		product = Product.create ({ 
-					title: Commerce.product_name,
-								category_id: Number.between(1, 9),
-								price: Number.decimal(3, 2),
-								product_url: "http://www.amazon.co.uk/",
-								photo: "http://ecx.images-amazon.com/images/I/51Nl%2B2DhSrL._AC_SY220_.jpg"})
-					
-					
+	product = Product.create ({ 
+		title: Commerce.product_name,
+		category_id: Number.between(1, 9),
+		price: Number.decimal(3, 2),
+		product_url: "http://www.amazon.co.uk/",
+		photo: "http://ecx.images-amazon.com/images/I/51Nl%2B2DhSrL._AC_SY220_.jpg"}),
+		user_id: users.sample.id,
+
+
+
 
 	p user.inspect
 	# user.encrypted_password = User.new({:password => password.encrypted_password})
-		
+
 end
